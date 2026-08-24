e29d503a8b6befb3883a6d7ae6bec0b4a51215d0
NicoG2006 <nicolas.garciap@uqvirtual.edu.co>
2026-08-24 13:14:25 -0500

    comunication.py

--- Proyecto_RdTp_Personal/comunication.py
+++ Proyecto_RdTp_Personal/comunication.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, HTTPException
+from fastapi import Response
 from pydantic import BaseModel
 from typing import Optional
 from radio_logic import astronomy_logic as astro
 from radio_logic import star_data as sd
+import struct
 
-app = FastAPI(title="RadioHunter || Nicolás García Parra")
+app = FastAPI(title=" D-Core Explorer || Nicolás García Parra")
 
 class stuctCoord(BaseModel):
     ra:float
@@ -14,6 +16,8 @@ current_angle = {
     "az": 0.0,
     "alt": 0.0,
 }
+
+active_mode = "IDLE"
 
 @app.get("/greet")
 def greet():
@@ -28,6 +32,8 @@ def greet():
 @app.post("/api/track/sun")
 def trackSun():
     try:
+        global active_mode 
+        active_mode="SUN"
         az, alt = astro.buscar_sol()
         current_angle["az"]=az
         current_angle["alt"]=alt
@@ -41,7 +47,13 @@ def trackSun():
 
 @app.post("/api/track/object")
 def trackObject(coord:stuctCoord):
+    global current_angle
+    global r,d
+    r = coord.ra
+    d = coord.dec
     try:
+        global active_mode 
+        active_mode = "OBJECT"
         az, alt = astro.rastrear(coord.ra, coord.dec)
         current_angle["az"]=az
         current_angle["alt"]=alt
@@ -55,6 +67,11 @@ def trackObject(coord:stuctCoord):
 @app.post("/api/track/savedObj")
 def track_savedOBJ(name:str):
     try:
+        global active_mode
+        global ra, dec
+        global n
+        n = name
+        active_mode = "SAVED_OBJ"
         coords= sd.buscar_obj(name)
         if (coords)==None:
             raise HTTPException(status_code=500, detail=str(f"{name} not exist on catalog"))
@@ -79,4 +96,14 @@ def save(name:str, ra:float, dec:float):
 
 @app.get("/api/target")
 def getTarget():
-    return current_angle
+    global active_mode
+    if(active_mode=="SUN"):
+       az, alt = astro.buscar_sol()
+    if(active_mode=="OBJECT"):
+        az , alt = astro.rastrear(r,d)
+    if(active_mode == "SAVED_OBJ"):
+        az, alt = astro.rastrear(ra, dec)
+        
+    data_bi = struct.pack('ff', float(az),float(alt))
+    print([hex(b) for b in data_bi])
+    return Response(content=data_bi, media_type="application/octet-stream")
